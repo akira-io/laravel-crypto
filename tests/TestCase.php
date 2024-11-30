2b8dd87364195f15eae5c812ec8a302c6392d9fc
kidiatoliny <kidiatoliny@users.noreply.github.com>
2024-11-30 21:26:59 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,7 +8,7 @@ use Akira\LaravelCrypto\LaravelCryptoServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
 
- class TestCase extends Orchestra
+class TestCase extends Orchestra
 {
     protected function setUp(): void
     {
