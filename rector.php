5ea4ef24ac6c08b81cff19a01ee451ea77519451
kid <kidiatoliny@akira-io.com>
2025-04-19 00:22:26 -0100

    chore: update composer.json for improved development dependencies; add rector configuration and enhance test workflow in run-tests.yml

---
+++ rector.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/src',
+        __DIR__.'/config',
+    ])
+    ->withSkip([
+        //  AddOverrideAttributeToOverriddenMethodsRector::class,
+    ])
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        privatization: true,
+        earlyReturn: true,
+        strictBooleans: true,
+    )
+    ->withPhpSets();
